c037798db141a515b6e8ba58345a499207851951
kratika0901 <kratikaverma3110@gmail.com>
2026-09-05 21:16:06 +0530

    Add Minus Two (Unrated)

---
+++ codeforces/Unrated/2259B - Minus Two.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define int long long
+#define double long double
+#define endl "\n"
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define inp_vec(x,v) for(auto &x : v) { cin>>x;}
+#define out_vec(x,v) for(auto x : v) 
+#define all(v) v.begin(),v.end()
+#define vi vector<int>
+using namespace std;
+
+const int MOD = 1e9 + 7; // 998244353
+
+// bool prime(int n){
+//     if(n<2) return false;
+//     for(int i=2;i<=sqrt(n);i++){
+//         if(n%i==0) return false;
+//     }
+//     return true;
+// }
+
+// int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+// int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+// int power(int base, int exp) {
+//     int res = 1; base %= MOD;
+//     while (exp > 0) {
+//         if (exp % 2 == 1) res = (res * base) % MOD;
+//         base = (base * base) % MOD;
+//         exp /= 2;
+//     }
+//     return res;
+// }
+
+void solve()
+{
+    int n; cin>>n;
+    vi v(n); inp_vec(x,v)
+    int odd=0,even=0,ans=0;
+    rep(i,n){
+        if(v[i]%2!=0) odd++;
+        // else if(v[i]%2!=0 && v[i]%4==0) ans++;
+        // else if(v[i]%4 !=0 && v[i]%2==0) even++;
+        else if(v[i]%4==0) ans++;
+        else if(v[i]%4==2) even++;
+    }
+    cout<<max({odd,even,ans});
+    return;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t; cin >> t;
+    while(t--)
+    {
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
+// ./\_/\.
+// (>^.^<)
+// /  )
