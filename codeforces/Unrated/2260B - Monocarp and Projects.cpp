676edcbe680b21f9b81c5bf4dfd21b09a5913251
kratika0901 <kratikaverma3110@gmail.com>
2026-09-08 21:33:29 +0530

    Add Monocarp and Projects (Unrated)

---
+++ codeforces/Unrated/2260B - Monocarp and Projects.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define int long long
+#define double long double
+#define endl "\n"
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define inp_vec(x,v) for(auto &x : v) { cin>>x;}
+#define out_vec(x,v) for(auto x : v) 
+#define all(v) v.begin(),v.end()
+#define vi vector<int>
+using namespace std;
+
+const int MOD = 1e9 + 7; // 998244353
+
+// bool prime(int n){
+//     if(n<2) return false;
+//     for(int i=2;i<=sqrt(n);i++){
+//         if(n%i==0) return false;
+//     }
+//     return true;
+// }
+
+// int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+// int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+// int power(int base, int exp) {
+//     int res = 1; base %= MOD;
+//     while (exp > 0) {
+//         if (exp % 2 == 1) res = (res * base) % MOD;
+//         base = (base * base) % MOD;
+//         exp /= 2;
+//     }
+//     return res;
+// }
+
+void solve()
+{
+    int x,y,k; cin>>x>>y>>k;
+    int sum=0;
+    int i=0;
+    
+    while(x+i<=(y-x) && i<k){
+        sum=sum+(y+i)%(x+i);
+        i++;
+    }
+
+    if(k>i)sum=sum+(y-x)*(k-i);
+    cout<<sum;
+    return;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t; cin >> t;
+    while(t--)
+    {
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
+// ./\_/\.
+// (>^.^<)
+// /  )
